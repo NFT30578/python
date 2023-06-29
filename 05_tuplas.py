118afc44ce50549d98c7e339573b8b5248b8f277
Bitkor <victor_menthalist@hotmail.com>
2023-06-28 22:23:44 -0400

    commit

---
+++ 05_tuplas.py
@@ -0,0 +1,18 @@
+#coleccion de diferentes datos que estan ordenadas y no se pueden cambiar(inmutables)
+
+tuple = ("hola",1 , "mundo")
+tuple2 = tuple ("hola", "casa", )
+
+tuple[0]    #tuple con elemento hola
+primerElemento = tuple[0]   #primer elemento guardado en variable primerElemento
+primerElemento
+
+lista = []
+lista2 = list()
+
+# funciones:
+
+""" 
+count
+index
+"""
